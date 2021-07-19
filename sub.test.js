6fd7516ef3daed888001bd4bd3d57ffcd11e6fd1
Chris Winter <chris-winter@live.de>
2021-07-19 17:20:40 +0200

    added more tests

--- sub.test.js
+++ sub.test.js
@@ -2,3 +2,11 @@ const subtraction = require('./sub');
 test('subtracts 4 - 2 to equal 2', () => {
    expect(subtraction(4, 2)).toBe(2);
 });
+
+test('subtracts 0 - 2 to equal -2', () => {
+   expect(subtraction(0, 2)).toBe(-2);
+});
+
+test('subtracts 0 - 0 to equal 0', () => {
+   expect(subtraction(0, 0)).toBe(0);
+});
